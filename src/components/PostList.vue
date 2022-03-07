6abe74e272cd7ca3f58d5060998fdcbae0445335
Ömer Hodo <omerhodo@gmail.com>
2022-03-07 15:54:06 +0300

    55

---
+++ src/components/PostList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-for="post in posts" :key="post.id">
+    <h3>{{ post.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["posts"],
+  setup(props) {
+    console.log(props.posts);
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,92 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <!-- 1 -->
-    <!-- <p>{{ name }} -- {{ role }}</p> -->
-    <!-- <p>p tag</p> -->
-    <!-- 3 -->
-    <!-- <p>{{ name }}</p>
-  <input type="text" v-model="name" />
-  <button @click="buttonClick">Click</button> -->
-    <!-- 4 -->
-    <!-- <p>{{ name }}--{{ rolesObject.role }}</p>
-    <button @click="buttonClick">Click</button> -->
-
-    <!-- 5 -->
-    <br />
-    <input type="text" v-model="search" />
-    <div v-for="name in founded" :key="name">
-      <p>{{ name }}</p>
-    </div>
-    <!-- 6 -->
-    <button @click="stop">Stop</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import { onMounted, ref, reactive, computed, watch, watchEffect } from "vue";
+import PostList from "../components/PostList.vue";
+
+import { ref } from "vue";
 
 export default {
   name: "HomeView",
-  components: {},
+  components: { PostList },
   setup() {
-    // 5
-    const names = ref(["omer", "hodo", "md", "hd", "yc"]);
-    const search = ref("");
-
-    const founded = computed(() => {
-      return names.value.filter((name) => name.includes(search.value));
-    });
-    // 6
-    const watchN = watch(search, () => {
-      console.log("watch start");
-    });
-
-    const watchEffectN = watchEffect(() => {
-      console.log("watchEffect start", names.value);
-      console.log("watchEffect start", search.value);
-    });
-
-    const stop = () => {
-      watchN();
-      watchEffectN();
-    };
-
-    return { names, search, founded, stop };
-
-    // 4
-    // let name = ref("hodo");
-    // let rolesObject = reactive({ role: "admin" });
-    // const buttonClick = () => {
-    //   name.value = "omer";
-    //   rolesObject.role = "user";
-    // };
-    // return { buttonClick, name, rolesObject };
-
-    // 3
-    // let name = ref("hodo");
-    // const buttonClick = () => {
-    //   name.value = "omer";
-    // };
-    // return { name, buttonClick };
-
-    // 2
-    // const information = ref("hodo");
-    // const buttonClick = () => {
-    //   console.log(information, information.value);
-    //   information.value = "button clicked";
-    // };
-    // return { buttonClick, information };
-
-    // 1
-    // let name = "hodo";
-    // let role = "admin";
-    // const buttonClick = () => {
-    //   console.log("button clicked");
-    // };
-    // return { name, role, buttonClick };
+    const posts = ref([
+      {
+        id: 1,
+        title: "Hello World",
+        body: "This is my first post",
+      },
+      {
+        id: 2,
+        title: "Second Post",
+        body: "This is my second post",
+      },
+    ]);
+    return { posts };
   },
 };
 </script>
